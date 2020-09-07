6ee5115409ed3fcafb4678456f0a2171960f564a
A-Tione <heyanggo@gmail.com>
2020-09-07 16:19:13 +0800

    node-fanyi init

---
+++ src/cli.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import * as commander from "commander";
+import {translate} from './main'
+const pkg = require('../package.json')
+const program = new commander.Command()
+
+program.
+    version(pkg.version)
+    .name('fy')
+    .usage('<English>')
+    .arguments('<English>')
+    .action((english) => {
+        translate(english)
+    });
+
+program.parse(process.argv)
